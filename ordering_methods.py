f6a50b5aaeebf0f526d79dcfdbb207934701040a
Roman Malinowski <roman.malinowski@mines-paristech.fr>
2022-12-30 00:37:57 +0100

    3D Histogram heuristic running

--- ordering_methods.py
+++ ordering_methods.py
@@ -152,16 +152,17 @@ def transfer_with_dependence_euclidian(img, img_smooth, img_ref, seed=42):
         print("\r%s / %s    " % (k, len(df_ref)), end="")
 
         df["d_p"] = np.sqrt((df["ch1"] - df_ref.loc[ind, "ch1"]) ** 2 +
-                              (df["ch2"] - df_ref.loc[ind, "ch2"]) ** 2 +
-                              (df["ch3"] - df_ref.loc[ind, "ch3"]) ** 2)
+                            (df["ch2"] - df_ref.loc[ind, "ch2"]) ** 2 +
+                            (df["ch3"] - df_ref.loc[ind, "ch3"]) ** 2)
 
         df["d_smooth_p"] = np.sqrt((df["smooth1"] - df_ref.loc[ind, "ch1"]) ** 2 +
-                                     (df["smooth2"] - df_ref.loc[ind, "ch2"]) ** 2 +
-                                     (df["smooth3"] - df_ref.loc[ind, "ch3"]) ** 2)
+                                   (df["smooth2"] - df_ref.loc[ind, "ch2"]) ** 2 +
+                                   (df["smooth3"] - df_ref.loc[ind, "ch3"]) ** 2)
 
         df = df.sort_values(by=["d_p", "d_smooth_p"])
 
-        img_transferred[df.loc[df.index[0], "row"], df.loc[df.index[0], "col"], :] = df_ref.loc[ind, ["ch1", "ch2", "ch3"]].to_numpy(
+        img_transferred[df.loc[df.index[0], "row"], df.loc[df.index[0], "col"], :] = df_ref.loc[
+            ind, ["ch1", "ch2", "ch3"]].to_numpy(
             dtype=int)
 
         df.drop(df.index[0], axis=0, inplace=True)
@@ -195,9 +196,21 @@ def compute_square_euclidian_distance_matrix(img, img_ref):
     return dist_img
 
 
-def transfer_with_dependence_euclidian_heuristic(img, img_smooth, img_ref, batch_size=400, seed=42):
-    assert img.shape == img_ref, "Method only works for same shape images." \
+def transfer_with_dependence_euclidian_heuristic(img, img_smooth, img_ref, batch_size=100, seed=42):
+    assert img.shape == img_ref.shape, "Method only works for same shape images." \
                                  "Resample them so they have the same shape"
+
+    if img.dtype == np.dtype('uint8'):
+        warnings.warn("img dtype is unsigned integer. Casting to int32 to compute euclidian distance")
+        img = img.astype(dtype=np.int32)
+
+    if img_ref.dtype == np.dtype('uint8'):
+        warnings.warn("img_ref dtype is unsigned integer. Casting to int32 to compute euclidian distance")
+        img_ref = img_ref.astype(dtype=np.int32)
+
+    if img_smooth.dtype == np.dtype('uint8'):
+        warnings.warn("img dtype is unsigned integer. Casting to int32 to compute euclidian distance")
+        img_smooth = img_smooth.astype(dtype=np.int32)
 
     n_pixels = np.product(img_ref.shape[:2])
     indices = pd.DataFrame(index=range(n_pixels), columns=["ind"], data=range(n_pixels))
@@ -209,18 +222,38 @@ def transfer_with_dependence_euclidian_heuristic(img, img_smooth, img_ref, batch
     # full_order: index = pixel in img, column = pixel in img_ref
     full_order = pd.DataFrame(index=range(n_pixels), columns=["ind"], data=np.zeros(n_pixels))
 
-    for k in range(n_pixels//batch_size+1):
+    for k in range(n_pixels // batch_size + 1):
+        print("\r%s / %s    " % (k+1, n_pixels // batch_size), end="")
+        """
+        Example:
+        indices = 
+            ind
+        0    4    
+        1    5
+        2    9
+        3   12
+        
+        indices_ref = 
+            ind
+        0    7
+        1    3
+        2    0
+        """
         # Splitting the random order of pixels in different batch for faster processing
-        i_min, i_max = k * batch_size, max((k + 1) * batch_size, n_pixels)
+        i_min, i_max = k * batch_size, min((k + 1) * batch_size, len(indices_ref))
         if i_min == i_max:
             i_max += 1
+        """
+        i_min, i_max = 0, 3
+        """
+        indices_ref_batch = indices_ref.loc[i_min:(i_max-1), "ind"].to_numpy()
+        """[7, 3, 0]"""
 
-        indices_ref_batch = indices_ref.loc[i_min:i_max, "ind"]
-
-        while len(indices_ref_batch) > 0:
+        while indices_ref_batch.size > 0:
             rows, cols = np.unravel_index(indices["ind"], img.shape[:2])
+            """[a, b, c, d], [A, B, C, D]"""
             rows_ref, cols_ref = np.unravel_index(indices_ref_batch, img_ref.shape[:2])
-
+            """[a', b', c'], [A', B', C']"""
             # Computing distance matrices
             dist_img = compute_square_euclidian_distance_matrix(img[rows, cols, :], img_ref[rows_ref, cols_ref, :])
             """
@@ -228,18 +261,21 @@ def transfer_with_dependence_euclidian_heuristic(img, img_smooth, img_ref, batch
              [5, 7, 9, 8],
              [3, 1, 1, 2]]
             """
-            dist_smooth_img = compute_square_euclidian_distance_matrix(img_smooth[rows, cols, :], img_ref[rows_ref, cols_ref, :])
+            dist_smooth_img = compute_square_euclidian_distance_matrix(img_smooth[rows, cols, :],
+                                                                       img_ref[rows_ref, cols_ref, :])
             """
-            [[4, 1, 2, 3],
+            [[4, 2, 1, 3],
              [6, 6, 6, 6],
-             [9, 0, 3, 0]]
+             [9, 3, 0, 0]]
             """
 
             # Finding the minimal distances for each row
-            dist_min = np.tile(dist_img.min(axis=1), (dist_img.shape[1], 1)).T
+            pixel_min_val = dist_img.min(axis=1)  # Min distance retained
+            """[0, 5, 1]"""
+            dist_min = np.tile(pixel_min_val, (dist_img.shape[1], 1)).T
             """
             [[0, 0, 0, 0],
-             [0, 0, 0, 0],
+             [5, 5, 5, 5],
              [1, 1, 1, 1]]
             """
 
@@ -253,16 +289,15 @@ def transfer_with_dependence_euclidian_heuristic(img, img_smooth, img_ref, batch
             # For distances that are equal, sorting by neighbour
             dist_smooth_img[~dist_min] = 200000  # More than dist_max=3*255**2
             """
-            [[2e5, 1, 2, 2e5],
+            [[2e5, 2, 1, 2e5],
              [6, 2e5, 2e5, 2e5],
-             [2e5, 0, 3, 2e5]]
+             [2e5, 3, 0, 2e5]]
             """
             pixel_min = np.argmin(dist_smooth_img, axis=1)  # array of shape (i_min - i_max,)
-            """[1, 0, 1]"""
-            pixel_min_val = dist_img[np.arange(pixel_min.size), pixel_min]  # Min distance retained
-            """[0, 5, 1]"""
+            """[2, 0, 2]"""
+
             unique, unique_counts = np.unique(pixel_min, return_counts=True)
-            """[0, 1], [1, 2]"""
+            """[0, 2], [1, 2]"""
             is_unique = np.isin(pixel_min, unique[unique_counts == 1])
             """[False, True, False]"""
 
@@ -277,12 +312,23 @@ def transfer_with_dependence_euclidian_heuristic(img, img_smooth, img_ref, batch
                 """[True, True, False]"""
 
             matches = np.isin(indices.index, pixel_min[is_unique])  # TODO The is_unique should be redundant
-
+            """[True, False, True, False]"""
             # full_order: index = pixel in img, column = pixel in img_ref
-            full_order.loc[indices[matches]["ind"], "ind"] = indices_ref_batch[is_unique]["ind"]
+            full_order.loc[indices.loc[pixel_min[is_unique], "ind"], "ind"] = indices_ref_batch[is_unique]
+            """full_order.loc[[9, 4], "ind"] = [7, 3]"""
 
             # Removing the pixels from img that have been chosen to the list of available pixels
             indices = indices[~matches]
             indices.reset_index(drop=True, inplace=True)
 
-            indices_ref_batch = indices_ref_batch[~matches]
+            # Removing the pixels from the batch of img_ref that have been assigned a match
+            indices_ref_batch = indices_ref_batch[~is_unique]
+
+    img_copy = img.copy()
+
+    rows, cols = np.unravel_index(full_order.index, img.shape[:2])
+    rows_ref, cols_ref = np.unravel_index(full_order["ind"].to_numpy(), img_ref.shape[:2])
+
+    img_copy[rows, cols, :] = img_ref[rows_ref, cols_ref, :]
+
+    return img_copy
